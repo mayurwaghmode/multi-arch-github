f2cf2487497fa4b9a37733da5f6e9b6fd5d29fb9
Mayur Waghmode <42777188+mayurwaghmode@users.noreply.github.com>
2022-08-09 19:23:30 +0530

    Update script-to-run-on-ppc64le.sh

--- script-to-run-on-ppc64le.sh
+++ script-to-run-on-ppc64le.sh
@@ -1,7 +1,11 @@
 ARCH=$(uname -m)
-rm -rf /root/power-runner-ssh
-git clone -b multi-arch-natively https://github.com/mayurwaghmode/multi-arch-github.git
-cd /root/power-runner-ssh
-buildah bud -f Dockerfile -t quay.io/mayurwaghmode111/gha-multi-arch:$ARCH .
-buildah login -u mayurwaghmode111+test -p X92E11Y7OV17QBLJRJT2URECDG11J5LY67BKFBAG5ASZMMWO9667YHO9LFOKCAGW quay.io
-podman push quay.io/mayurwaghmode111/gha-multi-arch:$ARCH
+if (( $# == 3 ))
+then
+    QUAY_REPO=$1
+    ROBOT_USER=$2
+	  ROBOT_TOKEN=$3
+fi
+
+buildah bud -f Dockerfile -t $QUAY_REPO:$ARCH .
+buildah login -u $ROBOT_USER=$2 -p ROBOT_TOKEN=$3 quay.io
+podman push $QUAY_REPO:$ARCH
